ecbccd5009d34ff6862e3ace286dfc624d15c91c
sanjeevan <sanjeevan.g0@gmail.com>
2025-01-01 19:30:57 +0530

    integrated supabse to the project

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  async function getCabinsData() {
+    let data = await getCabins();
+    console.log(data);
+  }
+  useEffect(() => {
+    getCabinsData();
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://rowgrkgkfczqrdjbrwmc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt=""
+      />
     </Row>
   );
 }
